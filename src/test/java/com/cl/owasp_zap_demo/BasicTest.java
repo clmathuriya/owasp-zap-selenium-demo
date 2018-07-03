2e9af2be7da4c1b7303b314543d630df8cf13e0e
Chhagan Lal Mathuriya <cmathuriya@mobileiron.com>
2018-07-03 22:16:14 +0530

    to try with chrome browser

--- src/test/java/com/cl/owasp_zap_demo/BasicTest.java
+++ src/test/java/com/cl/owasp_zap_demo/BasicTest.java
@@ -1,10 +1,10 @@
 package com.cl.owasp_zap_demo;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -12,11 +12,11 @@ import org.testng.annotations.Test;
 
 public class BasicTest {
   @Test
-  public void f() throws MalformedURLException {
+  public void chromeTest() throws MalformedURLException {
 
 		// TODO Auto-generated method stub
 		String PROXY = "localhost:8090";
-		System.setProperty("webdriver.gecko.driver", "geckodriver");
+		System.setProperty("webdriver.chrome.driver", new File("chromedriver_linux").getAbsolutePath());
 
 		org.openqa.selenium.Proxy proxy = new org.openqa.selenium.Proxy();
 		proxy.setHttpProxy(PROXY)
@@ -24,10 +24,33 @@ public class BasicTest {
 		     .setSslProxy(PROXY);
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability(CapabilityType.PROXY, proxy);
-		capabilities.setCapability("marionette", true);
+		//capabilities.setCapability("marionette", true);
+		capabilities.setCapability(CapabilityType.BROWSER_NAME, "chrome");
 
 		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
-		driver.get("http://localhost:8080/login?from=%2F");
+		driver.get("http://newtours.demoaut.com/");
+		driver.quit();
 		//SimpleZAPExample.main(null);
+  }
+  @Test(enabled=false)
+  public void firefoxTest() throws MalformedURLException {
+
+        // TODO Auto-generated method stub
+        String PROXY = "localhost:8090";
+        System.setProperty("webdriver.gecko.driver", new File("geckodriver_mac").getAbsolutePath());
+
+        org.openqa.selenium.Proxy proxy = new org.openqa.selenium.Proxy();
+        proxy.setHttpProxy(PROXY)
+             .setFtpProxy(PROXY)
+             .setSslProxy(PROXY);
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.PROXY, proxy);
+        capabilities.setCapability("marionette", true);
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "firefox");
+
+        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+        driver.get("http://newtours.demoaut.com/");
+        driver.quit();
+        //SimpleZAPExample.main(null);
   }
 }
